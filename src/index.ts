d5b6535ed864fc664677051909e9848c4c777042
ngotruonganh <ngotruonganh1901@gmail.com>
2023-08-22 02:30:45 +0700

    add home request

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import userRouter from '~/routes/users.router'
 import { defaultErrorHandler } from '~/middlewares/error.middlewares'
 import cors, { CorsOptions } from 'cors'
 import postRouter from '~/routes/posts.router'
+import httpStatus from "~/constants/httpStatus";
 
 const corsOptions: CorsOptions = {
   origin: '*',
@@ -21,6 +22,13 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Instaggram back end',
+    status: httpStatus.OK
+  })
+})
 
 app.use('/api/v1/users', userRouter)
 
