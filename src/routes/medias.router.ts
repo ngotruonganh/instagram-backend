832197ca0a320108f89c30e8f9fac76e38ed15f1
ngotruonganh <ngotruonganh1901@gmail.com>
2023-08-26 03:39:11 +0700

    add wraphandle router

--- src/routes/medias.router.ts
+++ src/routes/medias.router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { mediaController } from '~/controllers/mediaController'
+import {wrapHandleError} from "~/utils/handlerError";
 
 const mediaRouter = Router()
 
-mediaRouter.post('/upload-image', mediaController)
+mediaRouter.post('/upload-image',  wrapHandleError(mediaController))
 
 export default mediaRouter
